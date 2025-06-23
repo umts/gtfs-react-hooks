0291982dfaf9b5f345192bcb3358af8ff05de8e1
benmelz <ben@melz.me>
2025-06-23 15:11:27 -0400

    resolvers should be async

--- test/useRefresh.test.js
+++ test/useRefresh.test.js
@@ -17,24 +17,24 @@ describe('useRefresh', () => {
 
   it('periodically re-resolves according to the given timeout', async () => {
     const resolve = vi.fn()
-    resolve.mockImplementationOnce(() => 'old data')
+    resolve.mockImplementationOnce(async () => 'old data')
     const { result } = renderHook(() => useRefresh(resolve, 1000))
     await vi.waitFor(() => expect(result.current).toEqual('old data'))
 
-    resolve.mockImplementationOnce(() => 'new data')
+    resolve.mockImplementationOnce(async () => 'new data')
     vi.advanceTimersByTime(1000)
     await vi.waitFor(() => expect(result.current).toEqual('new data'))
     expect(resolve).toHaveBeenCalledTimes(2)
 
-    resolve.mockImplementationOnce(() => 'newer data')
+    resolve.mockImplementationOnce(async () => 'newer data')
     vi.advanceTimersByTime(1000)
     await vi.waitFor(() => expect(result.current).toEqual('newer data'))
     expect(resolve).toHaveBeenCalledTimes(3)
   })
 
   it('returns null when an error is raised', async () => {
-    const refresh = async () => { throw Error() }
-    const { result } = renderHook(() => useRefresh(refresh, 1000))
+    const resolve = async () => { throw Error() }
+    const { result } = renderHook(() => useRefresh(resolve, 1000))
     await vi.waitFor(() => expect(result.current).toBeNull())
   })
 })
