f4294dfd8752ae704128f0b5f1ab363a2f11138d
benmelz <ben@melz.me>
2025-06-23 13:14:05 -0400

    set up vitest

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import GtfsRealtimeReactHooks from '../lib/index.js'
+
+describe('index', () => {
+  it('exports something', () => {
+    expect(GtfsRealtimeReactHooks).toEqual('IMPLEMENT ME')
+  })
+})
