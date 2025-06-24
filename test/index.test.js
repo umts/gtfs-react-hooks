b6b5735dab0df954670a174a085dc3c75f65f292
Louis Pate <louispate@gmail.com>
2025-06-24 13:37:50 -0400

    feat: useGtfsRealtime hook (#9)

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,11 @@
 import { describe, expect, it } from 'vitest'
-import { useGtfsSchedule } from '../lib/index.js'
+import { useGtfsRealtime, useGtfsSchedule } from '../lib/index.js'
 
 describe('entrypoint', () => {
+  it('exports a useGtfsRealtime hook', () => {
+    expect(useGtfsRealtime).toBeTypeOf('function')
+  })
+
   it('exports a useGtfsSchedule hook', () => {
     expect(useGtfsSchedule).toBeTypeOf('function')
   })
