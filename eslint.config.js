4d5db2af78a902b688cbe65cc906819427d8a083
benmelz <ben@melz.me>
2025-06-26 11:16:54 -0400

    ignore coverage too dummy
    Co-authored-by: Louis Pate <louispate@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import neostandard from 'neostandard'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   ...neostandard({ env: ['browser'] }),
   { extends: [reactHooks.configs['recommended-latest']] },
 ])
