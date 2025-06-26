2d63fb0170ce29e580f95d810ab063645f74c53e
benmelz <ben@melz.me>
2025-06-26 13:02:51 -0400

    fix up schedule tests

--- test/useGtfsSchedule.test.js
+++ test/useGtfsSchedule.test.js
@@ -3,73 +3,54 @@ import fs from 'fs'
 import { describe, expect, it, vi } from 'vitest'
 import useGtfsSchedule from '../lib/useGtfsSchedule.js'
 
-import parsedSchedule1 from './fixtures/schedule1-parsed.json'
-import parsedSchedule2 from './fixtures/schedule2-parsed.json'
-import parsedSchedule3 from './fixtures/schedule3-parsed.json'
-
-const scheduleBuffer1 = fs.readFileSync('test/fixtures/schedule1-raw.zip')
-const scheduleBuffer2 = fs.readFileSync('test/fixtures/schedule2-raw.zip')
-const scheduleBuffer3 = fs.readFileSync('test/fixtures/schedule3-raw.zip')
+import parsedSchedule from './fixtures/schedule.json'
+const rawSchedule = fs.readFileSync('test/fixtures/schedule.zip')
 
 describe('useGtfsSchedule', () => {
   it('returns undefined when unresolved', async () => {
-    const resolve = async () => scheduleBuffer1
+    const resolve = async () => rawSchedule
     const { result } = renderHook(() => useGtfsSchedule(resolve, 1000))
+
     expect(result.current).toBeUndefined()
   })
 
   it('returns undefined when resolved to undefined', async () => {
     const resolve = vi.fn()
-    resolve.mockImplementationOnce(async () => scheduleBuffer1)
+    resolve.mockImplementationOnce(async () => rawSchedule)
     const { result } = renderHook(() => useGtfsSchedule(resolve, 1000))
-    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule1))
+    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule))
 
     resolve.mockImplementationOnce(async () => undefined)
     vi.advanceTimersByTime(1000)
+
     await vi.waitFor(() => expect(result.current).toBeUndefined())
   })
 
   it('returns parsed schedule data when resolved to a Uint8Array', async () => {
-    const resolve = async () => scheduleBuffer1
+    const resolve = async () => rawSchedule
     const { result } = renderHook(() => useGtfsSchedule(resolve, 1000))
-    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule1))
+
+    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule))
   })
 
-  it('periodically re-resolves according to the given timeout', async () => {
+  it('periodically re-resolves according to the given timeout and retry values', async () => {
     const resolve = vi.fn()
-    resolve.mockImplementationOnce(async () => scheduleBuffer1)
-    const { result } = renderHook(() => useGtfsSchedule(resolve, 1000))
-    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule1))
-    expect(resolve).toHaveBeenCalledTimes(1)
-
-    resolve.mockImplementationOnce(async () => scheduleBuffer2)
-    vi.advanceTimersByTime(1000)
-    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule2))
-    expect(resolve).toHaveBeenCalledTimes(2)
-
-    resolve.mockImplementationOnce(async () => scheduleBuffer3)
-    vi.advanceTimersByTime(1000)
-    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule3))
-    expect(resolve).toHaveBeenCalledTimes(3)
-  })
-
-  it('re-resolves according to the retry timeout when resolve fails', async () => {
-    const resolve = vi.fn()
-    resolve.mockImplementationOnce(async () => scheduleBuffer1)
+    resolve.mockImplementationOnce(async () => rawSchedule)
     const { result } = renderHook(() => useGtfsSchedule(resolve, 5000, 1000))
+    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule))
 
-    expect(result.current).toEqual(undefined)
-    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule1))
     expect(resolve).toHaveBeenCalledTimes(1)
 
     resolve.mockImplementationOnce(async () => undefined)
     vi.advanceTimersByTime(5000)
+
     await vi.waitFor(() => expect(result.current).toEqual(undefined))
     expect(resolve).toHaveBeenCalledTimes(2)
 
-    resolve.mockImplementationOnce(async () => scheduleBuffer1)
+    resolve.mockImplementationOnce(async () => rawSchedule)
     vi.advanceTimersByTime(1000)
-    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule1))
+
+    await vi.waitFor(() => expect(result.current).toEqual(parsedSchedule))
     expect(resolve).toHaveBeenCalledTimes(3)
   })
 })
