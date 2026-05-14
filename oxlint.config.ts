b98dd223e874ffef6632aeda59b5f74f793fffde
benmelz <ben@melz.me>
2026-05-14 10:50:40 -0400

    lint lib code

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from "oxlint";
 
 export default defineConfig({
   $schema: "./node_modules/oxlint/configuration_schema.json",
-  plugins: ["eslint", "unicorn", "react", "react-perf", "oxc", "import", "promise", "vitest"],
+  plugins: ["unicorn", "react", "react-perf", "oxc", "import", "promise", "vitest"],
   categories: {
     correctness: "error",
-    suspicious: "error",
+    suspicious: "warn",
     pedantic: "warn",
     perf: "error",
     restriction: "error",
+  },
+  rules: {
+    "no-undefined": "off",
+    "import/no-default-export": "off",
+    "oxc/no-async-await": "off",
+    "oxc/no-rest-spread-properties": "off",
   },
 });
