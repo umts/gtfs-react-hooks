a42ddfbb1dc09e93145496170e4167b0177e9da7
benmelz <ben@melz.me>
2026-05-22 11:03:53 -0400

    bundle smaller dependencies

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       formats: ["es"],
     },
     rollupOptions: {
-      external: ["change-case", "jszip", "gtfs-realtime-bindings", "papaparse", "react"],
+      external: ["gtfs-realtime-bindings", "react"],
     },
   },
   test: {
