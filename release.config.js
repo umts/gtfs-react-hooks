1a7b625a6af371b3500d576173e80ca4292038cc
benmelz <ben@melz.me>
2026-05-22 09:33:25 -0400

    update packages

--- release.config.js
+++ release.config.js
@@ -9,7 +9,6 @@ export default {
       "@semantic-release/git",
       {
         assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
-        // eslint-disable-next-line no-template-curly-in-string
         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
