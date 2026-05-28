d22eec7592ad82cf5a8fb9f4ddcb98c6eaadba6e
Aryanvakil <67795771+Aryanvkl@users.noreply.github.com>
2026-05-28 14:42:29 -0400

    feat: useGtfsScheduleCsv hook (#123)
    ---------
    
    Co-authored-by: benmelz <ben@melz.me>

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,10 @@
 import { describe, expect, it } from "vitest";
-import { useFetchResolver, useGtfsRealtime, useGtfsSchedule } from "../lib/index.js";
+import {
+  useFetchResolver,
+  useGtfsRealtime,
+  useGtfsSchedule,
+  useGtfsScheduleCsv,
+} from "../lib/index.js";
 
 describe("entrypoint", () => {
   it("exports a useGtfsRealtime hook", () => {
@@ -8,6 +13,10 @@ describe("entrypoint", () => {
 
   it("exports a useGtfsSchedule hook", () => {
     expect(useGtfsSchedule).toBeTypeOf("function");
+  });
+
+  it("exports a useGtfsScheduleCsv hook", () => {
+    expect(useGtfsScheduleCsv).toBeTypeOf("function");
   });
 
   it("exports a useFetchResolver hook", () => {
---
+++ test/useGtfsScheduleCSV.test.js
@@ -0,0 +1,55 @@
+import { renderHook } from "@testing-library/react";
+import fs from "node:fs";
+import { describe, expect, it, vi } from "vitest";
+import useGtfsScheduleCsv from "../lib/useGtfsScheduleCsv.js";
+
+import parsedFile from "./fixtures/routes.json";
+const rawFile = fs.readFileSync("test/fixtures/routes.txt");
+
+describe("useGtfsScheduleCsv", () => {
+  it("returns undefined when unresolved", () => {
+    const { result } = renderHook(() => useGtfsScheduleCsv(() => Promise.resolve(rawFile), 1000));
+
+    expect(result.current).toBeUndefined();
+  });
+
+  it("returns undefined when resolved to undefined", async () => {
+    const resolve = vi.fn();
+    resolve.mockImplementationOnce(() => Promise.resolve(rawFile));
+    const { result } = renderHook(() => useGtfsScheduleCsv(resolve, 1000));
+    await vi.waitFor(() => expect(result.current).toEqual(parsedFile.routes));
+
+    resolve.mockImplementationOnce(() => Promise.resolve());
+    vi.advanceTimersByTime(1000);
+
+    await vi.waitFor(() => expect(result.current).toBeUndefined());
+  });
+
+  it("returns parsed route data when resolved to a csv file", async () => {
+    const { result } = renderHook(() => useGtfsScheduleCsv(() => Promise.resolve(rawFile), 1000));
+    await vi.waitFor(() => expect(result.current).toEqual(parsedFile.routes));
+  });
+
+  it("periodically re-resolves according to the given timeout and retry values", async () => {
+    const resolve = vi.fn();
+    resolve.mockImplementationOnce(() => Promise.resolve(rawFile));
+    const { result } = renderHook(() => useGtfsScheduleCsv(resolve, 5000, 1000));
+
+    await vi.waitFor(() => expect(result.current).toEqual(parsedFile.routes));
+    expect(resolve).toHaveBeenCalledTimes(1);
+
+    // create a problem and wait for the next regular refresh
+    resolve.mockImplementationOnce(() => Promise.resolve());
+    vi.advanceTimersByTime(5000);
+
+    await vi.waitFor(() => expect(result.current).toEqual(undefined));
+    expect(resolve).toHaveBeenCalledTimes(2);
+
+    // fix problem and wait for the next retry refresh
+    resolve.mockImplementationOnce(() => Promise.resolve(rawFile));
+    vi.advanceTimersByTime(1000);
+
+    await vi.waitFor(() => expect(result.current).toEqual(parsedFile.routes));
+    expect(resolve).toHaveBeenCalledTimes(3);
+  });
+});
