7288dc37de4e58b323e31e5031447360ff27a95f
Aryanvakil <aryanvkl82@gmail.com>
2026-05-28 12:43:40 -0400

    Add hook and tests for getting single csv files

---
+++ test/useGtfsFile.test.js
@@ -0,0 +1,55 @@
+import { renderHook } from "@testing-library/react";
+import fs from "node:fs";
+import { describe, expect, it, vi } from "vitest";
+import useGtfsFile from "../lib/useGtfsFile.js";
+
+import parsedFile from "./fixtures/routes.json";
+const rawFile = fs.readFileSync("test/fixtures/routes.txt");
+
+describe("useGtfsFile", () => {
+  it("returns undefined when unresolved", () => {
+    const { result } = renderHook(() => useGtfsFile(() => Promise.resolve(rawFile), 1000));
+
+    expect(result.current).toBeUndefined();
+  });
+
+  it("returns undefined when resolved to undefined", async () => {
+    const resolve = vi.fn();
+    resolve.mockImplementationOnce(() => Promise.resolve(rawFile));
+    const { result } = renderHook(() => useGtfsFile(resolve, 1000));
+    await vi.waitFor(() => expect(result.current).toEqual(parsedFile));
+
+    resolve.mockImplementationOnce(() => Promise.resolve());
+    vi.advanceTimersByTime(1000);
+
+    await vi.waitFor(() => expect(result.current).toBeUndefined());
+  });
+
+  it("returns parsed route data when resolved to a csv file", async () => {
+    const { result } = renderHook(() => useGtfsFile(() => Promise.resolve(rawFile), 1000));
+    await vi.waitFor(() => expect(result.current).toEqual(parsedFile));
+  });
+
+  it("periodically re-resolves according to the given timeout and retry values", async () => {
+    const resolve = vi.fn();
+    resolve.mockImplementationOnce(() => Promise.resolve(rawFile));
+    const { result } = renderHook(() => useGtfsFile(resolve, 5000, 1000));
+
+    await vi.waitFor(() => expect(result.current).toEqual(parsedFile));
+    expect(resolve).toHaveBeenCalledTimes(1);
+
+    // create a problem and wait for the next regular refresh
+    resolve.mockImplementationOnce(() => Promise.resolve());
+    vi.advanceTimersByTime(5000);
+
+    await vi.waitFor(() => expect(result.current).toEqual(undefined));
+    expect(resolve).toHaveBeenCalledTimes(2);
+
+    // fix problem and wait for the next retry refresh
+    resolve.mockImplementationOnce(() => Promise.resolve(rawFile));
+    vi.advanceTimersByTime(1000);
+
+    await vi.waitFor(() => expect(result.current).toEqual(parsedFile));
+    expect(resolve).toHaveBeenCalledTimes(3);
+  });
+});
